06028ea6f2559812b3d99b3992716b5061ee26b1
Bekoavci1 <c1811006@student.cankaya.edu.tr>
2023-08-28 21:55:57 +0300

    create menu and profile

---
+++ constants/images.js
@@ -0,0 +1,39 @@
+const hero = require('../assets/deneme.jpg')
+const friend1 = require('../assets/deneme.jpg')
+const friend2 = require('../assets/deneme.jpg')
+const friend3 = require('../assets/deneme.jpg')
+const friend4 = require('../assets/deneme.jpg')
+const friend5 = require('../assets/deneme.jpg')
+const user1 = require('../assets/deneme.jpg')
+const user2 = require('../assets/deneme.jpg')
+const user3 = require('../assets/deneme.jpg')
+const user4 = require('../assets/deneme.jpg')
+const user5 = require('../assets/deneme.jpg')
+const user6 = require('../assets/deneme.jpg')
+const user7 = require('../assets/deneme.jpg')
+const post1 = require('../assets/deneme.jpg')
+const post2 = require('../assets/deneme.jpg')
+const post3 = require('../assets/deneme.jpg')
+const post4 = require('../assets/deneme.jpg')
+const post5 = require('../assets/deneme.jpg')
+
+export default {
+    hero,
+    friend1,
+    friend2,
+    friend3,
+    friend4,
+    friend5,
+    user1,
+    user2,
+    user3,
+    user4,
+    user5,
+    user6,
+    user7,
+    post1,
+    post2,
+    post3,
+    post4,
+    post5,
+}
--- screens/index.js
+++ screens/index.js
@@ -1,9 +1,19 @@
 import Login from "./Login";
 import Welcome from "./Welcome";
 import Signup from "./Signup";
+import Feed from './Feed'
+import Create from './Create'
+import Profile from './Profile'
+import Notifications from './Notifications'
+import Chat from './Chat'
 
 export {
     Login,
     Welcome,
-    Signup
+    Signup,
+    Feed,
+    Create,
+    Profile,
+    Notifications,
+    Chat
 }
